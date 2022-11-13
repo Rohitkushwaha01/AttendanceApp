3782334f8717e7a5a22a6e4e74d0e74f5187fc9a
Rohit Kushwaha <rohitkushwahac@gmail.com>
2022-11-13 23:12:52 +0530

    change styling font

---
+++ public/js/time.js
@@ -0,0 +1,7 @@
+const time = document.querySelector('.time');
+
+
+setInterval(() => {
+    let date = new Date();
+    time.innerText = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+}, 100);
